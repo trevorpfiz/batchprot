3d432f60d7b4c42726ca48e4c6b0c8783b1b54ef
trevorpfiz <elektrikspark@gmail.com>
2025-07-03 13:11:03 -0500

    fix: disable ssl when connecting to local postgres

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -6,15 +6,17 @@ export default {
   dialect: 'postgresql',
   out: './migrations',
   dbCredentials: {
-    ssl: false, // Local Postgres does not support SSL
-    // ssl: {
-    //   rejectUnauthorized: false,
-    // },
     host: Resource.MyPostgres.host,
     port: Resource.MyPostgres.port,
     user: Resource.MyPostgres.username,
     password: Resource.MyPostgres.password,
     database: Resource.MyPostgres.database,
+    ssl:
+      Resource.MyPostgres.host !== 'localhost'
+        ? {
+            rejectUnauthorized: false,
+          }
+        : false,
   },
   tablesFilter: ['prot_*'],
   casing: 'snake_case',
